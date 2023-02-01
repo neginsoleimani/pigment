1ff3daf1f0378eefe0f466af44cc00640651ecc9
Mohammad Drikvand <mohammaddrikvand217@gmail.com>
2023-02-01 19:46:05 +0330

    Add Feature
    Add Feature

---
+++ src/Components/Feature/Feature.jsx
@@ -0,0 +1,26 @@
+import './feature.css'
+
+function Feature() {
+  return (
+    <div className="feature__container">
+      <div className="feature__title-box">
+        <h1>چرا پیگمنت؟</h1>
+      </div>
+      <div className="grid-container-top">
+        <div>1</div>
+        <div>2</div>
+      </div>
+      <div className="grid-container-mid">
+        <div>1</div>
+        <div>2</div>
+      </div>
+      <div className="grid-container-bot">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
+}
+
+export default Feature;
